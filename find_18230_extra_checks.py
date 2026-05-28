86fb5809db6ea1968d299756f9d800b5da45f445
Vlad <you@example.com>
2026-05-28 16:31:02 +0300

    sglks

--- find_18230_extra_checks.py
+++ find_18230_extra_checks.py
@@ -1,21 +1,30 @@
-# Доп. проверки: структура tariff_comiss + глобальный поиск 18230 в tariff_calc
+# Поиск 18230 в tariff_comiss.c_value
 
-# 1) Колонки tariff_comiss
-sql_descr = "describe ods.scd1_z_r2_tariff_comiss"
-with imp:
-    imp.execute('set MEM_LIMIT=8g')
-    descr_df = imp.fetch(sql_descr)
-descr_df
+target_tariff_ids = ['42429264236', '53599758030']
+tids_in = ', '.join([f"'{x}'" for x in target_tariff_ids])
 
-
-# 2) Глобальный поиск 18230 в tariff_calc.c_summa (если такая колонка есть)
-sql_calc = """
-select id, c_summa
-from ods.scd1_z_r2_tariff_calc
-where c_summa between 18229 and 18231
+# 1) tariff_comiss для нашего договора
+sql_comiss = f"""
+select id, c_value, c_max_value, c_min_value, c_currency
+from ods.scd1_z_r2_tariff_comiss
+where cast(id as string) in ({tids_in})
 """
 with imp:
     imp.execute('set MEM_LIMIT=8g')
-    calc_df = imp.fetch(sql_calc)
-print(len(calc_df) if calc_df is not None else 0)
-calc_df
+    comiss_df = imp.fetch(sql_comiss)
+print(len(comiss_df) if comiss_df is not None else 0)
+comiss_df
+
+
+# 2) Глобальный поиск 18230 в tariff_comiss.c_value
+sql_comiss_search = """
+select id, c_value
+from ods.scd1_z_r2_tariff_comiss
+where c_value like '%18230%' or c_value like '%18 230%'
+limit 20
+"""
+with imp:
+    imp.execute('set MEM_LIMIT=8g')
+    search_df = imp.fetch(sql_comiss_search)
+print(len(search_df) if search_df is not None else 0)
+search_df
